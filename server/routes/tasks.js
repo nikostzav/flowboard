d78f6c4010c30beab8f857a7d00f6f01db3d73d8
nikostzav <niktzavell@gmail.com>
2026-07-17 14:01:17 +0300

    Add Get /:project_id/tasks to list project tasks

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -33,4 +33,27 @@ router.post("/", authMiddleware, async (req, res) => {
   }
 });
 
+router.get("/", authMiddleware, async (req, res) => {
+  const { project_id } = req.params;
+  try {
+    const membership = await pool.query(
+      `SELECT workspace_members.* FROM workspace_members
+            JOIN projects ON projects.workspace_id = workspace_members.workspace_id
+            WHERE projects.id = $1 AND workspace_members.user_id = $2`,
+      [project_id, req.user.userId],
+    );
+    if (membership.rows.length === 0) {
+      return res.status(403).json({ message: "No authorization" });
+    }
+    const result = await pool.query(
+      "SELECT * FROM tasks WHERE project_id = $1",
+      [project_id],
+    );
+
+    res.status(200).json({ tasks: result.rows });
+  } catch (err) {
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
 module.exports = router;
